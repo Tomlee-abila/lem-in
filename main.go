ce6d527773a4934a5724a90ad61da3ab7bfb2d8d
svictor <victorshayo677@gmail.com>
2025-01-31 13:45:01 +0300

    feat: add simulateAnts function to simulate ant movement along the path

--- main.go
+++ main.go
@@ -134,22 +134,26 @@ func findShortestPath(antFarm *AntFarm) []string {
 }
 
 
+func simulateAnts(antFarm *AntFarm, path []string) {
+	ants := make([]int, antFarm.Ants)
+	for i := range ants {
+		ants[i] = 1 // Ants start at the first room in the path
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	for {
+		moves := []string{}
+		for i := range ants {
+			if ants[i] < len(path) {
+				moves = append(moves, fmt.Sprintf("L%d-%s", i+1, path[ants[i]]))
+				ants[i]++
+			}
+		}
+		if len(moves) == 0 {
+			break
+		}
+		fmt.Println(strings.Join(moves, " "))
+	}
+}
 
 
 
