7467f1619aa06df7a76c0ae15558915c09a34f3e
svictor <victorshayo677@gmail.com>
2025-01-31 13:42:57 +0300

    feat: add findShortestPath function to find the shortest path using BFS

--- main.go
+++ main.go
@@ -102,14 +102,36 @@ func parseRoom(line string) *Room {
 }
 
 
+func findShortestPath(antFarm *AntFarm) []string {
+	// Use BFS to find the shortest path
+	visited := make(map[string]bool)
+	queue := [][]string{{antFarm.Start}}
 
+	for len(queue) > 0 {
+		path := queue[0]
+		queue = queue[1:]
+		lastRoom := path[len(path)-1]
 
+		if lastRoom == antFarm.End {
+			return path
+		}
 
+		for _, tunnel := range antFarm.Tunnels {
+			rooms := strings.Split(tunnel, "-")
+			if rooms[0] == lastRoom && !visited[rooms[1]] {
+				visited[rooms[1]] = true
+				newPath := append(path, rooms[1])
+				queue = append(queue, newPath)
+			} else if rooms[1] == lastRoom && !visited[rooms[0]] {
+				visited[rooms[0]] = true
+				newPath := append(path, rooms[0])
+				queue = append(queue, newPath)
+			}
+		}
+	}
 
-
-
-
-
+	return nil
+}
 
 
 
