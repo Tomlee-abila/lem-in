cd0d95e285fb74fd108dea6ef5d0acf2c638a4a2
Tomlee Abila <tomleeabila@gmail.com>
2025-02-06 09:59:04 +0300

    fix: print output in correct format

--- parser/parse_input.go
+++ parser/parse_input.go
@@ -23,29 +23,38 @@ func ParseInput(filename string) (*types.AntFarm, error) {
 
 	// Read number of ants
 	if !scanner.Scan() {
-		return nil, fmt.Errorf("invalid data format, no ants found")
+		return nil, fmt.Errorf("ERROR: invalid data format, no ants found")
 	}
 	ants, err := strconv.Atoi(scanner.Text())
-	if err != nil || ants <= 0 {
-		return nil, fmt.Errorf("invalid data format, invalid number of ants")
+	fileContent := ""
+	fileContent += strconv.Itoa(ants)
+	if err != nil || ants <= 1 {
+		return nil, fmt.Errorf("ERROR: invalid data format, invalid number of ants")
 	}
 	antFarm.Ants = ants
 
 	// Read rooms and tunnels
 	for scanner.Scan() {
 		line := scanner.Text()
+
+		if strings.Count(line, "#") != 1{
+			fileContent += "\n"+line
+		}
+
 		if strings.HasPrefix(line, "##start") {
 			if !scanner.Scan() {
-				return nil, fmt.Errorf("invalid data format, no start room found")
+				return nil, fmt.Errorf("ERROR: invalid data format, no start room found")
 			}
 			startRoom := ParseRoom(scanner.Text())
+			fileContent += "\n"+startRoom.Name+" "+strconv.Itoa(startRoom.X)+" "+strconv.Itoa(startRoom.Y)
 			antFarm.Start = startRoom.Name
 			antFarm.Rooms[startRoom.Name] = startRoom
 		} else if strings.HasPrefix(line, "##end") {
 			if !scanner.Scan() {
-				return nil, fmt.Errorf("invalid data format, no end room found")
+				return nil, fmt.Errorf("ERROR: invalid data format, no end room found")
 			}
 			endRoom := ParseRoom(scanner.Text())
+			fileContent += "\n"+endRoom.Name+" "+strconv.Itoa(endRoom.X)+" "+strconv.Itoa(endRoom.Y)
 			antFarm.End = endRoom.Name
 			antFarm.Rooms[endRoom.Name] = endRoom
 		} else if strings.Contains(line, "-") {
@@ -58,8 +67,10 @@ func ParseInput(filename string) (*types.AntFarm, error) {
 
 	// Validate start and end rooms
 	if antFarm.Start == "" || antFarm.End == "" {
-		return nil, fmt.Errorf("invalid data format, start or end room missing")
+		return nil, fmt.Errorf("ERROR: invalid data format, start or end room missing")
 	}
 
+	fmt.Println(fileContent)
 	return antFarm, nil
 }
+
